d5a8c2fe5f1ba9dde307f0b0909c52bb457c475e
Cody deVries <cdevries@northern.co>
2021-08-01 22:01:58 -0400

    Show some of the last viewed jokes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import fetchDadJoke from "./api/fetchDadJoke";
 
 function App() {
   const [dadJoke, setDadJoke] = useState("");
-  const [existingJokeIds, setExistingJokeIds] = useState([]);
+  const [viewedJokes, setViewedJokes] = useState([]);
+  console.log(viewedJokes);
 
   const updateDadJoke = async () => {
-    await fetchDadJoke(existingJokeIds).then((result) => {
+    await fetchDadJoke(viewedJokes).then((result) => {
       setDadJoke(result.joke);
-      setExistingJokeIds([...existingJokeIds, result.id]);
+      const newJoke = {
+        id: result.id,
+        joke: result.joke,
+      };
+
+      setViewedJokes([...viewedJokes, newJoke]);
     });
   };
 
@@ -25,6 +31,23 @@ function App() {
         <button className="App-button" onClick={updateDadJoke}>
           Give Me a New Joke!
         </button>
+        {viewedJokes.length > 1 && (
+          <div class="last-viewed">
+            <span>Last viewed jokes...</span>
+            <ul>
+              {viewedJokes
+                .slice(0)
+                .reverse()
+                .map((joke, index) => {
+                  if (index === 0 || index > 5) {
+                    return "";
+                  }
+
+                  return <li key={joke.id}>{joke.joke}</li>;
+                })}
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
